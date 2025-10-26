449aab3d3a6579333a3785c317f8dc5aaf47d2de
DeathNeroTV <runge.roman@gmail.com>
2025-10-26 12:34:11 +0100

    changed to discord auth

--- src/plugins/discord-auth/server/config.ts
+++ src/plugins/discord-auth/server/config.ts
@@ -1,6 +1,6 @@
 export const DiscordAuthConfig = {
-    APPLICATION_ID: '',
-    SERVER_ID: '', // leave empty if do not use whitelist | REQUIRED rebar-discord
-    WHITELIST_ROLE_ID: '', // leave empty if do not use whitelist | REQUIRED rebar-discord
-    SESSION_EXPIRE_TIME_IN_MINUTES: 3,
+    APPLICATION_ID: '1284150599490998376',
+    SERVER_ID: '1284160784364081295', // leave empty if do not use whitelist | REQUIRED rebar-discord
+    WHITELIST_ROLE_ID: '1428323459632595024', // leave empty if do not use whitelist | REQUIRED rebar-discord
+    SESSION_EXPIRE_TIME_IN_MINUTES: 1440,
 }
--- src/plugins/discord-bot/server/config.ts
+++ src/plugins/discord-bot/server/config.ts
@@ -1,13 +1,13 @@
 import {GatewayIntentBits} from "discord.js";
 
 export const DiscordConfig = {
-    CLIENT_ID: "",
-    APPLICATION_ID: "",
-    SERVER_ID: "",
-    BOT_TOKEN: "",
+    CLIENT_ID: "1284150599490998376",
+    APPLICATION_ID: "1284150599490998376",
+    SERVER_ID: "1284160784364081295",
+    BOT_TOKEN: "MTI4NDE1MDU5OTQ5MDk5ODM3Ng.GQl6OX.sDL7qnAPl44eSE9bzcAXdOnLyac6tqu9iLtRyo",
     BOT_STATES: [
-        "[_player_count_/250] players connected",
-        "_vehicle_count_ vehicles spawned",
+        "[_player_count_/250] Spieler verbunden",
+        "_vehicle_count_ Fahrzeuge erstellt",
         "sooo funny"
     ],
     BOT_WEBSITE_URL: '',
