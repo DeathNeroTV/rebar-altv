048ae21909b605796f937064d7ac3565eef96092
Roman Runge <runge.roman@gmail.com>
2025-10-26 12:43:42 +0100

    Clear DiscordConfig credentials
    Removed sensitive information from Discord configuration.

--- src/plugins/discord-bot/server/config.ts
+++ src/plugins/discord-bot/server/config.ts
@@ -1,10 +1,10 @@
 import {GatewayIntentBits} from "discord.js";
 
 export const DiscordConfig = {
-    CLIENT_ID: "1284150599490998376",
-    APPLICATION_ID: "1284150599490998376",
-    SERVER_ID: "1284160784364081295",
-    BOT_TOKEN: "MTI4NDE1MDU5OTQ5MDk5ODM3Ng.GQl6OX.sDL7qnAPl44eSE9bzcAXdOnLyac6tqu9iLtRyo",
+    CLIENT_ID: "",
+    APPLICATION_ID: "",
+    SERVER_ID: "",
+    BOT_TOKEN: "",
     BOT_STATES: [
         "[_player_count_/250] Spieler verbunden",
         "_vehicle_count_ Fahrzeuge erstellt",
